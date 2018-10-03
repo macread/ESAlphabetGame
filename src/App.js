e3b5c04690a421dd31368c9bc79d568448cde1ab
James 'Mac' Read <james.macgregor.read@gmail.com>
2018-10-02 21:48:12 -0600

    get the counts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,106 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'typeface-roboto';
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
 
 class App extends Component {
+
+  constructor() {
+    super()
+    
+    this.state = {
+      enteredText: '',
+      alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      letters: [],
+      count: [],
+    }
+  }
+
+  componentDidMount() {
+    //put the alphbet into an array (let the computer do the work)
+    const letters = this.state.alphabet.split('');
+    this.setState({letters});
+  }
+
+  handleChange(val) {
+    this.setState({
+      enteredText: val,
+    });
+  };
+
+  countLetterOccurances() {
+    //initialize the count array
+    let count = this.state.letters.map(count => count = 0);
+    //put the entered text into an arry
+    const letters = this.state.enteredText.split('');
+    //now count the letters
+    letters.forEach(letter => {
+      const idx = this.state.letters.indexOf(letter.toUpperCase());
+      count[idx]++;
+    });
+    //update state so we can display the counts
+    this.setState({count})
+  }
+
   render() {
+    const { classes } = this.props;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">A Way Fun Alphabet Game</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro"></p>
+
+      
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="alphabet"
+            label="Enter your text here"
+            multiline
+            rowsMax="10"
+            value={this.state.enteredText}
+            onChange={( e ) => this.handleChange( e.target.value )}
+            className={classes.textField}
+            margin="normal"
+            helperText="Enter some text and I will count the number of times each letter is used."
+            variant="filled"
+          />
+          
+          <Button 
+            variant="contained" 
+            color="primary" 
+            className={classes.button}
+            onClick={() => this.countLetterOccurances()}
+          >
+            Count
+          </Button>
+        </form>
+        
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
